c6929ae35aaafcb7050ee52fc7ce057912f4d7b1
Paul Tela <paul@paultela.com>
2017-01-22 10:43:02 -0500

    Webpack config for sass and react

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './sass/main.scss';
+
+ReactDOM.render(
+	<h1>Hello, world!</h1>,
+	document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	entry: './app/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	devtool: 'cheap-module-eval-source-map',
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: "babel-loader"
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				enforce: 'pre',
+				loader: 'eslint-loader'
+			},
+			{
+				test: /\.s?css$/,
+				loader: ExtractTextPlugin.extract({
+					loader: [
+						{
+							loader: 'css-loader',
+							query: {
+								sourceMap: true,
+							}
+						},
+						{
+							loader: 'postcss-loader',
+							query: {
+								sourceMap: true,
+							}
+						},
+						{
+							loader: 'resolve-url-loader',
+							query: {
+								sourceMap: true,
+							}
+						},
+						{
+							loader: 'sass-loader',
+							query: {
+								sourceMap: true,
+							}
+						}
+					]
+				})
+			},
+			{
+				test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png$|\.jpg$|\.gif$/,
+				loader: 'file-loader'
+			}
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin({
+			filename: 'bundle.css',
+			disable: false,
+			allChunks: true,
+		})
+	]
+}
